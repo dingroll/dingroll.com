3eb39cf29563deade1d8a2b6687bbb0f17101ba0
Stuart P. Bentley <stuart@testtrack4.com>
2015-06-03 14:22:15 +0000

    Include scope in controller when adding post

--- app/controllers/messages/create.js
+++ app/controllers/messages/create.js
@@ -4,6 +4,7 @@ module.exports = function messageCreator(pool) {
   return function(message) {
     return pool.runQuery(r.table('messages').insert({
       body: message.body,
+      scope: message.scope,
       creation: r.now()
     }));
   };
